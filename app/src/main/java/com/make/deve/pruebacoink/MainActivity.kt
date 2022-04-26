f6bc4eefbad28e0d93227334c57a9c24c258b116
Maicol Alarcon <maicolalarcon93@gmail.com>
2022-04-26 10:58:59 -0500

    Feat: DataBinding & RegisterActivity

--- app/src/main/java/com/make/deve/pruebacoink/MainActivity.kt
+++ app/src/main/java/com/make/deve/pruebacoink/MainActivity.kt
@@ -1,13 +1,22 @@
 package com.make.deve.pruebacoink
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.databinding.DataBindingUtil
+import com.make.deve.pruebacoink.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         installSplashScreen()
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.registerBtn.setOnClickListener {
+            Toast.makeText(this, " dadsd", Toast.LENGTH_SHORT).show()
+        }
     }
 }
